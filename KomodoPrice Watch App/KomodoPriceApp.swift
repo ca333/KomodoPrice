307dd94da299b60216ff5cde46cd9fd1475ade7a
Kadan Stadelmann <ca333@keemail.me>
2023-03-18 00:36:09 +0100

    Initial Commit

---
+++ KomodoPrice Watch App/KomodoPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomodoPriceApp.swift
+//  KomodoPrice Watch App
+//
+//  Created by Kadan Stadelmann on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct KomodoPrice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

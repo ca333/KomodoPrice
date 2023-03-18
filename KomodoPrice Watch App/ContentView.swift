47f99756d95da84499ee8d68c2537c21a4a5ac8a
Kadan Stadelmann <ca333@keemail.me>
2023-03-18 08:41:32 +0100

    prototype

--- KomodoPrice Watch App/ContentView.swift
+++ KomodoPrice Watch App/ContentView.swift
@@ -1,21 +1,52 @@
-//
-//  ContentView.swift
-//  KomodoPrice Watch App
-//
-//  Created by Kadan Stadelmann on 18/03/2023.
-//
-
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @StateObject private var priceFetcher = PriceFetcher()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("KMD Price")
+                .font(.largeTitle)
+            Text("\(priceFetcher.price, specifier: "%.5f") USD")
+                .font(.title)
+                .padding()
         }
-        .padding()
+        .onAppear {
+            priceFetcher.fetchPrice()
+        }
+    }
+}
+
+struct Coin: Codable {
+    let komodo: KMDPrice
+}
+
+struct KMDPrice: Codable {
+    let usd: Double
+}
+
+class PriceFetcher: ObservableObject {
+    @Published var price: Double = 0.0
+    private var cancellable: AnyCancellable?
+
+    private let url = URL(string: "https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=usd")!
+
+    func fetchPrice() {
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: Coin.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error fetching KMD price: \(error.localizedDescription)")
+                }
+            }, receiveValue: { [weak self] coin in
+                self?.price = coin.komodo.usd
+            })
     }
 }
 
